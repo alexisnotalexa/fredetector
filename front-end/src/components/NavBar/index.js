ff7797e9aa5c762ecac7136efa6d28df1b06fc5f
Alexis Okamura <alexiskokamura@gmail.com>
2019-08-17 21:29:28 -0700

    setup basic navbar functionality

--- front-end/src/components/NavBar/index.js
+++ front-end/src/components/NavBar/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class NavBar extends Component {
+  onLinkClick = () => this.props.history.push('/about');
+
   render() {
     return (
       <div className={'navbar--container'}>
         <h1 className={'navbar--link'}>FREDETECTOR</h1>
-        <span className={'navbar--link about'}>/ About</span>
+        <span className={'navbar--link about'} onClick={this.onLinkClick}>/ About</span>
       </div>
     );
   }
--- front-end/src/containers/App/index.js
+++ front-end/src/containers/App/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
 
+import About from '../../components/About';
+import Landing from '../../components/Landing';
 import NavBar from '../../components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar history={this.props.history} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);
