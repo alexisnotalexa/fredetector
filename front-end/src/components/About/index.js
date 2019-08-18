c690d9a387eff34ba58caccb2a061c0ff9d9f947
Alexis Okamura <alexiskokamura@gmail.com>
2019-08-17 21:54:51 -0700

    add styles to about page

--- front-end/src/components/About/index.js
+++ front-end/src/components/About/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './styles.css';
 
 const About = () => {
   return (
-    <div>
-      <p>
+    <div className={'about--container'}>
+      <p className={'about--text'}>
       Lorem ipsum dolor amet pork belly drinking vinegar typewriter lumbersexual air plant vegan next level pop-up ethical blog gluten-free scenester. Craft beer palo santo iceland twee asymmetrical mustache normcore. 90's banh mi mustache scenester, keytar beard tbh. Vexillologist shoreditch everyday carry taiyaki chia. Yuccie venmo irony, lomo iPhone humblebrag tacos plaid vice unicorn letterpress whatever. Lumbersexual 90's vape selvage occupy.
       <br /><br />
-Synth chillwave shabby chic farm-to-table. Hammock hexagon affogato, distillery asymmetrical kogi +1 you probably haven't heard of them beard marfa coloring book. YOLO meh trust fund kale chips cronut taiyaki swag jean shorts gastropub pok pok roof party banh mi. Beard forage vaporware single-origin coffee polaroid migas, mustache lomo vegan pabst health goth portland prism flexitarian. Messenger bag cloud bread keffiyeh church-key. Health goth tilde 8-bit leggings beard, tbh skateboard fam palo santo artisan vaporware ramps portland lomo. Af migas stumptown flannel edison bulb cronut raw denim selfies meditation occupy.
+      Synth chillwave shabby chic farm-to-table. Hammock hexagon affogato, distillery asymmetrical kogi +1 you probably haven't heard of them beard marfa coloring book. YOLO meh trust fund kale chips cronut taiyaki swag jean shorts gastropub pok pok roof party banh mi. Beard forage vaporware single-origin coffee polaroid migas, mustache lomo vegan pabst health goth portland prism flexitarian. Messenger bag cloud bread keffiyeh church-key. Health goth tilde 8-bit leggings beard, tbh skateboard fam palo santo artisan vaporware ramps portland lomo. Af migas stumptown flannel edison bulb cronut raw denim selfies meditation occupy.
       <br /><br />
-Wayfarers marfa godard, locavore hoodie kitsch pug readymade offal put a bird on it shoreditch chillwave forage squid food truck. 3 wolf moon ennui man bun celiac migas. Etsy mumblecore enamel pin live-edge austin bicycle rights you probably haven't heard of them stumptown blog. Austin cornhole activated charcoal jianbing shaman, cred celiac air plant polaroid sustainable you probably haven't heard of them migas whatever.
+      Wayfarers marfa godard, locavore hoodie kitsch pug readymade offal put a bird on it shoreditch chillwave forage squid food truck. 3 wolf moon ennui man bun celiac migas. Etsy mumblecore enamel pin live-edge austin bicycle rights you probably haven't heard of them stumptown blog. Austin cornhole activated charcoal jianbing shaman, cred celiac air plant polaroid sustainable you probably haven't heard of them migas whatever.
       </p>
     </div>
   );
