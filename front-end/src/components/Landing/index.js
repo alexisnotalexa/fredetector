45030896d7437f8bf63b3e83d3bfb801ce7943d6
Alexis Okamura <alexiskokamura@gmail.com>
2019-08-18 01:17:35 -0700

    add simple switch between screens

--- front-end/src/components/Landing/index.js
+++ front-end/src/components/Landing/index.js
@@ -4,11 +4,17 @@ import Fred from './Fred';
 import Fredetector from './Fredetector';
 
 export default class Landing extends Component {
+  state = {
+    showDetector: false
+  };
+
+  onClick = () => this.setState({ showDetector: !this.state.showDetector });
   render() {
     return (
-      <div className={'landing--container'}>
-        {/* <Fred /> */}
-        <Fredetector />
+      <div className={'landing--container'} onClick={this.onClick}>
+        {this.state.showDetector
+          ? <Fredetector />
+          : <Fred />}
       </div>
     );
   }
